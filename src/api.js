5b6168644d137b422bbc8e3c29972a69dcb82b87
Shlomi Yahbes <99732661+syahbes@users.noreply.github.com>
2022-07-21 05:53:17 +0300

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '51f7ab2a29mshd7c3754e7596373p119969jsn76801c85b90f',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d4fda89507788a61cb9e3b2469dd9c53";
